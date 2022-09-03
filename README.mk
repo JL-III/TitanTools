3c83cb57e4e4a21db2c5c54cb09f2834220b6f95
Jesse Lopez III <83399599+JL-III@users.noreply.github.com>
2022-09-02 19:12:11 -0500

    Update README.mk

--- README.mk
+++ README.mk
@@ -1,1 +1,15 @@
-Titan enchants is a custom enchantment plugin.
+Titan enchants is a custom enchantment plugin for Theatria.
+
+Players can toggle through enchantments by holding shift and right clicking while holding an imbued or charged titan tool.
+
+Players can charge their tools with power crystals by taking a powercrystal and placing them on top of their titan tool inside their inventory UI.
+
+Custom enchantments include:
+  Titan Picks:
+    1: 1x1 block break with auto collect
+    2: 3x3 block break
+    3: 3x3 block break with auto collect
+  Titan Shovel:
+    1: Delete 1x1 block
+    2: Delete 3x3 blocks
+    3: Delete 5x5 blocks
