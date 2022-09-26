fe1641124b8279fc9d31fe7678781e420a021075
JL-III <jesselopez1162@gmail.com>
2022-09-26 11:56:32 -0500

    Added logging and cooldown
    Logging and cooldown for charging titan tool added.

--- src/main/java/com/nessxxiii/titanenchants/listeners/enchantmentManager/ChargeManagement.java
+++ src/main/java/com/nessxxiii/titanenchants/listeners/enchantmentManager/ChargeManagement.java
@@ -2,10 +2,9 @@ package com.nessxxiii.titanenchants.listeners.enchantmentManager;
 
 import com.nessxxiii.titanenchants.items.ItemInfo;
 import net.kyori.adventure.text.Component;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.Particle;
-import org.bukkit.Sound;
+import net.kyori.adventure.text.TextComponent;
+import org.bukkit.*;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,6 +13,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.List;
+import java.util.Objects;
 
 import static com.nessxxiii.titanenchants.util.TitanEnchantEffects.depletedChargeEffect;
 
@@ -25,20 +25,43 @@ public class ChargeManagement implements Listener {
     public static void applyCharge(InventoryClickEvent event){
         Player player = (Player) event.getWhoClicked();
         if (event.getCurrentItem() == null) return;
+
         ItemStack itemOnCursor = player.getItemOnCursor();
         ItemStack itemClicked = event.getCurrentItem();
         if (itemOnCursor.getItemMeta() == null) return;
         int numberOfCharge = itemOnCursor.getAmount();
         if (itemClicked.getType() == Material.AIR) return;
         if (!ItemInfo.isPowerCrystal(itemOnCursor)) return;
-        if (!ItemInfo.isTitanTool(itemClicked)) return;
-        if (!ItemInfo.isAllowedTitanType(itemClicked)) return;
-        if (ItemInfo.isImbued(itemClicked)) return;
+        if (ItemInfo.isPowerCrystal(itemClicked)) return;
+
+        if (!ItemInfo.isTitanTool(itemClicked)) {
+            return;
+        }
+        if (!ItemInfo.isAllowedTitanType(itemClicked)) {
+            return;
+        }
+        if (ItemInfo.isImbued(itemClicked)) {
+            return;
+        }
+        Material cooldown = Material.COD_SPAWN_EGG;
+        if (player.hasCooldown(cooldown)) {
+            player.sendActionBar(Component.text(ChatColor.DARK_RED + "You must wait " + ChatColor.YELLOW + (player.getCooldown(cooldown) / 20) + ChatColor.DARK_RED + " seconds before attempting to use a powercrystal."));
+            return;
+        }
+        player.setCooldown(cooldown, 100);
         addChargeLore(itemClicked,numberOfCharge * 100);
         player.getItemOnCursor().setAmount(0);
         player.getWorld().spawnParticle(Particle.FIREWORKS_SPARK,player.getEyeLocation(),100);
         player.getWorld().playSound(player.getLocation(), Sound.BLOCK_CONDUIT_ACTIVATE,10, 1);
         event.setCancelled(true);
+        Bukkit.getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE + "*-----------------------------------------------------------------------*");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GREEN + player.getName() + ChatColor.AQUA + " tried to add a power crystal.");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "Completed attempt to charge Titan Tool.");
+        Bukkit.getConsoleSender().sendMessage(itemClicked.displayName());
+        for (Component component : Objects.requireNonNull(itemClicked.getItemMeta().lore())) {
+            Bukkit.getConsoleSender().sendMessage(component);
+        }
+        Bukkit.getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE + "*-----------------------------------------------------------------------*");
     }
 
     public static void addChargeLore(ItemStack item, Integer amount){
@@ -96,7 +119,8 @@ public class ChargeManagement implements Listener {
                 item.setItemMeta(meta);
             } else
                 loreList.set(chargeIndex,ItemInfo.ANCIENT_CHARGE + " " + remainingCharge);
-            player.sendActionBar(Component.text(ChatColor.ITALIC + "§x§F§F§0§0§4§CPowerLvl: " + ChatColor.GREEN + amountTaken + " " + ChatColor.ITALIC + "§x§F§F§0§0§4§CCharge: " + ChatColor.YELLOW + (remainingCharge >= 1 ? remainingCharge : 0)));
+            player.sendActionBar(Component.text(ChatColor.ITALIC + "§x§F§F§0§0§4§CPowerLvl: " + ChatColor.GREEN + amountTaken + " "
+                    + ChatColor.ITALIC + "§x§F§F§0§0§4§CCharge: " + ChatColor.YELLOW + (remainingCharge >= 1 ? remainingCharge : 0)));
             ItemMeta meta = item.getItemMeta();
             meta.setLore(loreList);
             item.setItemMeta(meta);
