718e61bc06c6ca44a68256548c3c63c6b7be0d0b
JL-III <jesselopez1162@gmail.com>
2024-01-20 20:19:08 -0600

    update

---
+++ src/main/java/com/nessxxiii/titantools/events/PowerCrystalDropEvent.java
@@ -0,0 +1,18 @@
+package com.nessxxiii.titantools.events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class PowerCrystalDropEvent extends Event {
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLERS;
+    }
+}
--- src/main/java/com/nessxxiii/titantools/listeners/tools/ExcavatorBlockBreak.java
+++ src/main/java/com/nessxxiii/titantools/listeners/tools/ExcavatorBlockBreak.java
@@ -1,14 +1,15 @@
 package com.nessxxiii.titantools.listeners.tools;
 
+import com.nessxxiii.titantools.events.PowerCrystalDropEvent;
 import com.nessxxiii.titantools.events.tools.ExcavatorBlockBreakEvent;
 import com.nessxxiii.titantools.items.ItemCreator;
 import com.nessxxiii.titantools.util.Utils;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-
-import static com.nessxxiii.titantools.util.Utils.getRandomNumber;
+import org.bukkit.inventory.ItemStack;
 
 public class ExcavatorBlockBreak implements Listener {
 
@@ -16,31 +17,22 @@ public class ExcavatorBlockBreak implements Listener {
     public void onExcavatorBlockBreak(ExcavatorBlockBreakEvent event) {
         Block block = event.getDropLocation().getBlock();
         block.setType(Material.AIR);
-        int randomNumber = getRandomNumber(1,1000);
-        Utils.sendPluginMessage(event.getPlayer(), "RNG result: " + randomNumber);
+        int randomNumber = Utils.getRandomNumber(1,1000);
+        event.getPlayerLocation().getWorld().dropItemNaturally(event.getDropLocation(), getPowerCrystalType(randomNumber));
+        Bukkit.getPluginManager().callEvent(new PowerCrystalDropEvent());
+    }
 
+    public ItemStack getPowerCrystalType(int randomNumber) {
+        ItemStack powerCrystal = ItemCreator.powerCrystalCommon;
         if (randomNumber > 995) {
-            event.getPlayerLocation().getWorld().dropItemNaturally(event.getDropLocation(), ItemCreator.powerCrystalUltra);
-            return;
+            powerCrystal = ItemCreator.powerCrystalUltra;
+        } else if (randomNumber > 990) {
+            powerCrystal = ItemCreator.powerCrystalEpic;
+        } else if (randomNumber > 985) {
+            powerCrystal = ItemCreator.powerCrystalSuper;
+        } else if (randomNumber > 900) {
+            powerCrystal = ItemCreator.powerCrystalUncommon;
         }
-        if (randomNumber > 990) {
-            event.getPlayerLocation().getWorld().dropItemNaturally(event.getDropLocation(), ItemCreator.powerCrystalEpic);
-            return;
-        }
-        if (randomNumber > 985) {
-            event.getPlayerLocation().getWorld().dropItemNaturally(event.getDropLocation(), ItemCreator.powerCrystalSuper);
-            return;
-        }
-        if (randomNumber > 900) {
-            event.getPlayerLocation().getWorld().dropItemNaturally(event.getDropLocation(), ItemCreator.powerCrystalUncommon);
-            return;
-        }
-        if (randomNumber <= 10) {
-            for (int i = 0; i < randomNumber; i++){
-                event.getPlayerLocation().getWorld().dropItemNaturally(event.getDropLocation(), ItemCreator.powerCrystalCommon);
-            }
-        } else {
-            event.getPlayerLocation().getWorld().dropItemNaturally(event.getDropLocation(), ItemCreator.powerCrystalCommon);
-        }
+        return powerCrystal;
     }
 }
